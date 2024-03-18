3dbf5faa3de936310f25a4d1d74c986c0f2f9c23
Nhi Nguyen <unhing.1901@gmail.com>
2024-03-18 21:09:10 +0700

    Configs

--- app/src/main/java/com/nguyenuyennhi/charmsiestore/activity/MainActivity.java
+++ app/src/main/java/com/nguyenuyennhi/charmsiestore/activity/MainActivity.java
@@ -1,8 +1,10 @@
-package com.nguyenuyennhi.charmsiestore;
+package com.nguyenuyennhi.charmsiestore.activity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+
+import com.nguyenuyennhi.charmsiestore.R;
 
 public class MainActivity extends AppCompatActivity {
 
